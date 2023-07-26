0d3a1cfd91df787f4012b71cf2847515ccc50ee4
Ivan Berengeno <ivan.beren.mdp@gmail.com>
2023-07-26 20:46:02 -0300

    contador1

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-function add(){
-  console.log("Hola")
-  setCount(5)
-}
+  const stock = 13;
+  const [count, setCount] = useState(0);
+  function add() {
+    console.log("Hola");
+    if (count < stock) {
+      setCount(count + 1);
+    }
+  }
+  function remove() {
+    console.log();
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  }
+  function cero() {
+    setCount(0)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="conteiner">
+      <div className="conteiner">
+        <button onClick={remove}>-</button>
+        <h1>
+          {count}/{stock}
+        </h1>
+        <button onClick={add}>+</button>
       </div>
-      <h1>Vite + React</h1>
-      <button onClick={add}>Probar</button>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="conteiner">
+        <button onClick={cero}>
+          0
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
