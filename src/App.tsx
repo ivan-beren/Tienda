a365c0a8270fac47702dd812022f7d506488f844
Ivan Berengeno <ivan.beren.mdp@gmail.com>
2023-08-06 14:36:13 -0300

    it begins to add boucher tickets

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,19 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Counter } from "./components/counter";
+import { CounterContainer } from "./components/counter/container";
+import { CodeInput } from "./components/code-input";
 
 function App() {
-  return <div>hola
-    <Counter price={10} stock={3} name='ivan' imagen='https://images.pexels.com/photos/1741206/pexels-photo-1741206.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></Counter>
-    <Counter price={20} stock={15} name='beren' imagen='https://images.pexels.com/photos/2361952/pexels-photo-2361952.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></Counter>
-    <Counter price={30} stock={25} name='dota' imagen='https://images.pexels.com/photos/1835008/pexels-photo-1835008.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></Counter>
-//sacar las etiquetas "counter" y llevarlas a otro archivo como corresponde
-  </div>
-  
+  return(
+    <div>
+      <CounterContainer></CounterContainer>
+      <CodeInput></CodeInput>
+    </div>
+  ) 
 }
+
+
   
 
 export default App;
---
+++ src/components/code-input/index.tsx
@@ -0,0 +1,22 @@
+//componente de inserte su codigo de descuento aqui y un boton
+export const CodeInput=()=>{
+    const SECRET="dota";
+
+function ticket() {
+    console.log("Hola word");
+
+
+    }
+
+
+    return(
+        <div>
+            <button onClick={ticket}>
+                Enviar
+            </button>
+            <input type="text" />
+            //extraer valores de input
+            //armar code input
+        </div>
+    )
+}
